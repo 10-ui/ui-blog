1c0d9d6879bed39abe2953db84c915d7bb92aad3
Sentaro <ui-10@outlook.jp>
2024-02-14 23:49:54 +0900

    Darkモード実装、DB連携の準備完了、UI改善、iconのimport元を変更

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import Favicon from '/public/favicon/cat.ico';
+import { ThemeProvider } from '@/components/theme-provider';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,7 +19,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange>
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,32 @@
 import { Card } from '@/components/ui/card';
-import { Github, Twitter } from 'lucide-react';
+import { ModeToggle } from '@/components/ui/mode-toggle';
+import {
+  TwitterLogoIcon,
+  GitHubLogoIcon,
+} from '@radix-ui/react-icons';
 import Image from 'next/image';
 import Link from 'next/link';
 
 export default function Home() {
   return (
     <main className="min-h-screen p-14 md:p-20 lg:p-24">
-      <div className="flex justify-between items-center pr-4">
+      <div className="flex justify-between items-center">
         <h1 className="font-bold text-3xl">10-ui Blog</h1>
-        <div className="flex gap-2">
+        <div className="flex gap-2 items-center">
           <Link
             href={'https://twitter.com/10_uis'}
             target="_blank">
-            <Twitter />
+            <TwitterLogoIcon width={25} height={25} />
           </Link>
           <Link
             href={'https://github.com/10-ui'}
             target="_blank">
-            <Github />
+            <GitHubLogoIcon width={25} height={25} />
           </Link>
+          <ModeToggle />
         </div>
       </div>
-      <Card className="mt-5 w-full grid grid-cols-2 lg:grid-cols-3 grid-rows-1 gap-2 p-4">
+      <div className="mt-5 w-full grid grid-cols-2 lg:grid-cols-3 grid-rows-1 gap-4">
         <Card className="w-full p-3">
           <Image
             className="rounded-lg"
@@ -112,7 +117,7 @@ export default function Home() {
             UI development.
           </p>
         </Card>
-      </Card>
+      </div>
     </main>
   );
 }
